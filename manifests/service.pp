e5cd65d91f798aaa576efe664880060efaadc80b
Martin Schinz <m.schinz@gmail.com>
2015-01-11 17:43:43 +0000

    make init file executable under centos

--- manifests/service.pp
+++ manifests/service.pp
@@ -12,17 +12,26 @@ class kafka::service inherits kafka {
     default               => undef
   }
 
+  $startup_file_mode = $::os ? {
+    /(?i)(redhat|centos)/ => "0755",
+    /(?i)(debian|ubuntu)/ => "0644",
+    default               => undef
+  }
+
+
   file { $startup_install_path:
     content => $startup_file_content,
-    mode => "0644",
+    mode => $startup_file_mode,
     alias => 'kafka-init',
     require => File[$conf_file],
   }
 
   if $::os =~ /(?i)(centos|redhat)/{
     exec { 'chkconfig --add kafka':
-      path   => ["/sbin"],
-      before => Service["kafka"]
+      path    => ["/sbin"],
+      cwd     => "/etc/rc.d/init.d",
+      before  => Service["kafka"],
+      require => File["$startup_install_path"]
     }
   }
 
